9041e7527aebfb43d889ec01aa8ef4eed7b7397f
Rogier Verkaik <rogierv@gmail.com>
2018-11-15 07:35:01 +0100

    test

---
+++ src/Api.js
@@ -0,0 +1,33 @@
+// https://www.taniarascia.com/getting-started-with-react/
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    data: [2, 3, 4]
+  };
+
+  componentDidMount() {
+    const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
+
+    fetch(url)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          data: result
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+
+    return <ul>{result}</ul>;
+  }
+}
+
+export default App;
